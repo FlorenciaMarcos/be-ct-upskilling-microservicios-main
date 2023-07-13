45f60f87229f6741db2395a062f26c65f00ac13c
FlorenciaMarcos <florcita6221@gmail.com>
2023-07-13 19:39:24 -0300

    listo1

---
+++ star_wars_api/characters/src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const controllers = require("../controllers");
+const middlewares = require("../middlewares");
+
+const router = Router();
+
+router.get("/", controllers.getCharacters);
+
+//router.get("/characters", controllers.getCharacters);
+router.post("/", middlewares.characterValidation, controllers.createCharacter);
+
+module.exports = router;
